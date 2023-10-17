b1ec5287a6296036d2cfba4a9a5e0321acad9671
RedBug <o.adrian.rodrigues@gmail.com>
2023-10-17 11:26:46 -0300

    created project

---
+++ src/main/java/com/redbug/exemple/WebservicemongoApplication.java
@@ -0,0 +1,13 @@
+package com.redbug.exemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicemongoApplication.class, args);
+	}
+
+}
