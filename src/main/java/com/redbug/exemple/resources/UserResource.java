1f8368c3b0ab04bc5a1b8412eba73ccbcb1691e3
RedBug <o.adrian.rodrigues@gmail.com>
2023-10-17 11:46:54 -0300

    created User model and User resource

---
+++ src/main/java/com/redbug/exemple/resources/UserResource.java
@@ -0,0 +1,30 @@
+package com.redbug.exemple.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.redbug.exemple.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	//or @GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		User mary = new User("1", "Mary Jane", "maryjane@gmail.com");
+		User alex = new User("2", "Alex Ross", "alexross@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(mary, alex));
+		
+		return ResponseEntity.ok().body(list);
+
+	}
+
+}
