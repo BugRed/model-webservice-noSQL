3601f5abfe0bc69b1e1abac0b07c47aa4fbe6f25
RedBug <o.adrian.rodrigues@gmail.com>
2023-10-18 14:58:18 -0300

    Project author data with DTO

--- src/main/java/com/redbug/exemple/config/Instantiation.java
+++ src/main/java/com/redbug/exemple/config/Instantiation.java
@@ -10,40 +10,40 @@ import org.springframework.context.annotation.Configuration;
 
 import com.redbug.exemple.domain.Post;
 import com.redbug.exemple.domain.User;
+import com.redbug.exemple.dto.AuthorDTO;
 import com.redbug.exemple.repository.PostRepository;
 import com.redbug.exemple.repository.UserRepository;
-
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou pra SP abraços", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bomdia", "To lokona já", maria);
 
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou pra SP abraços",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bomdia", "To lokona já", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
+
 	}
 
 }
