7e6037a92e4b43a351579b0da4d7d2c8eb39220e
RedBug <o.adrian.rodrigues@gmail.com>
2023-10-17 22:06:57 -0300

    Find user with ID

--- src/main/java/com/redbug/exemple/services/UserService.java
+++ src/main/java/com/redbug/exemple/services/UserService.java
@@ -1,12 +1,14 @@
 package com.redbug.exemple.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.redbug.exemple.domain.User;
 import com.redbug.exemple.repository.UserRepository;
+import com.redbug.exemple.services.exeption.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
 }
