b1c78013c1720d2fdc9eed6d4f8875d553e69ad3
RedBug <o.adrian.rodrigues@gmail.com>
2023-10-18 16:07:29 -0300

    created query method

--- src/main/java/com/redbug/exemple/repository/PostRepository.java
+++ src/main/java/com/redbug/exemple/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.redbug.exemple.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.redbug.exemple.domain.Post;
@@ -12,6 +13,10 @@ public interface PostRepository extends MongoRepository<Post, String>{
 
 	//query methods criados pelo spring
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	//query method personalisado
+	@Query("{ 'title': { $regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
 }
 
 
