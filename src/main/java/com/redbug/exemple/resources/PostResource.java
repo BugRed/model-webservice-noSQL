179dd25723ab6bc83286428034798e0c5e8dffaf
RedBug <o.adrian.rodrigues@gmail.com>
2023-10-18 15:24:04 -0300

    endpoint for posts with id

--- src/main/java/com/redbug/exemple/resources/PostResource.java
+++ src/main/java/com/redbug/exemple/resources/PostResource.java
@@ -1,69 +1,26 @@
 package com.redbug.exemple.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.redbug.exemple.domain.User;
-import com.redbug.exemple.dto.UserDTO;
-import com.redbug.exemple.services.UserService;
+import com.redbug.exemple.domain.Post;
+import com.redbug.exemple.services.PostService;
 
 @RestController
-@RequestMapping(value = "/post")
+@RequestMapping(value = "/posts")
 public class PostResource {
 	
 	@Autowired
-	private UserService service;
-
-	@RequestMapping(method=RequestMethod.GET)
-	//or @GetMapping
-	public ResponseEntity<List<UserDTO>> findAll() {
-		List<User> list = service.findAll();
-		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDto);
-
-	}
+	private PostService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
-		User obj = service.findById(id);
-		return ResponseEntity.ok().body(new UserDTO(obj));
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
-	// or @PostMapping
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
-		User obj = service.fromDTO(objDTO);
-		obj = service.insert(obj);
-		//pegando o caminho do novo obj criado
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-	}
-	
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable String id){
-		service.delete(id);
-		return ResponseEntity.noContent().build();
-	}
-	
-	
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id){
-		User obj = service.fromDTO(objDTO);
-		obj.setId(id);
-		obj = service.update(obj);
-		return ResponseEntity.noContent().build();
-	}
-
 }
