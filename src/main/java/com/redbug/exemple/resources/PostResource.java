1acda1a6fc49ee02b36563213c2fe922fd1cccfa
RedBug <o.adrian.rodrigues@gmail.com>
2023-10-18 15:17:12 -0300

    Endpoint return posts of the users

--- src/main/java/com/redbug/exemple/resources/PostResource.java
+++ src/main/java/com/redbug/exemple/resources/PostResource.java
@@ -13,14 +13,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.redbug.exemple.domain.Post;
 import com.redbug.exemple.domain.User;
 import com.redbug.exemple.dto.UserDTO;
 import com.redbug.exemple.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/post")
+public class PostResource {
 	
 	@Autowired
 	private UserService service;
@@ -65,13 +64,6 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
-	}
-	
-	
-	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
-	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
-		User obj = service.findById(id);
-		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
